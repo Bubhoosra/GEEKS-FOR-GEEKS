3ac6c220c8340011f6862d81f5b896d0f5c01a23
Bubhoosra <120300546+Bubhoosra@users.noreply.github.com>
2023-12-06 22:22:21 +0530

    Updated Solution - GfG to GitHub

--- Hard/Word Ladder I/word-ladder-i.cpp
+++ Hard/Word Ladder I/word-ladder-i.cpp
@@ -5,31 +5,32 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
 public:
-    int wordLadderLength(string startWord, string targetWord, vector<string>& wordlist) {
+    int wordLadderLength(string startWord, string targetWord, vector<string>& wordList) {
         // Code here
         queue<pair<string,int>>q;
         q.push({startWord,1});
-        unordered_set<string>st(wordlist.begin(),wordlist.end());
+        unordered_set<string>st(wordList.begin(),wordList.end());
         st.erase(startWord);
         while(!q.empty()){
             string word=q.front().first;
-            int steps=q.front().second;
+            int count=q.front().second;
             q.pop();
-            if (word==targetWord){
-                return steps;
-            }
+            if (word==targetWord)  return count;
             for(int i=0;i<word.size();i++){
                 char original=word[i];
-                
-                for(char ch='a';ch<='z';ch++){
-                    word[i]=ch;
-                    if (st.find(word)!=st.end()){
-                        st.erase(word);
-                        q.push({word,steps+1});
-                    }
+            for(char ch='a';ch<='z';ch++){
+                word[i]=ch;
+                if (st.find(word)!=st.end()){
+                    st.erase(word);
+                    q.push({word,count+1});
                 }
-                word[i]=original;
+                
             }
+            word[i]=original;
+            
+                
+            }
+            
         }
         return 0;
         
