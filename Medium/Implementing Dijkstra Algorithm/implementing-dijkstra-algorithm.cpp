6759187ddec53d7da5e8160058c75b9ca1fdf0de
Bubhoosra <120300546+Bubhoosra@users.noreply.github.com>
2023-12-14 22:01:17 +0530

    Updated Solution - GfG to GitHub

--- Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
+++ Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
@@ -10,60 +10,30 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
-        vector<int> distance(V , 1e9);
-        distance[S] = 0;
-        minHeap.push({0 , S});
-        
-        while(!minHeap.empty()){
-            pair<int, int> p = minHeap.top();
-            minHeap.pop();
-            
-            int d = p.first;
-            int node = p.second;
-            
-            for(auto it : adj[node]){
-                int neighbour = it[0];
-                int edgewEIGHT = it[1];
-                
-                int newD = d + edgewEIGHT;
-                if(newD < distance[neighbour]){
-                    // found better distance
-                    minHeap.push({ newD , neighbour});
-                    distance[neighbour] = newD;
+        set<pair<int,int>>s;
+        vector<int>dist(V,1e9);
+        s.insert({0,S});
+        dist[S]=0;
+        while(!s.empty()){
+            auto it=*(s.begin());
+            int node=it.second;
+            int dis=it.first;
+            s.erase(it);
+            for(auto it:adj[node]){
+                int adjNode=it[0];
+                int edge=it[1];
+                if(dis+edge<dist[adjNode]){
+                    if(dist[adjNode]!=1e9){
+                        s.erase({dist[adjNode],adjNode});
+                    }
+                    dist[adjNode]=dis+edge;
+                    s.insert({dist[adjNode],adjNode});
                 }
             }
         }
-        
-        return distance;
-    }
+        return dist;
         // Code here
-    //     priority_queue<pair<int,int>,vector<pair<int,int>>,
-    //     greater<pair<int,int>>>q;
-    //     vector<int> dist(V);
-    //     for(int i=0;i<V;i++){
-    //         dist[i]=1e9;
-    //     }
-    //     dist[S]=0;
-    //     q.push({0,S});
-    //     while(!q.empty()){
-    //         int node=q.top().first;
-    //         int dis=q.top().second;
-    //         q.pop();
-    //         for(auto it:adj[node]){
-    //             if(dis+it[1]<dist[it[0]]){
-    //                 dist[it[0]]=dis+it[1];
-    //                 q.push({dist[it[0]],it[0]});
-    //             }
-    //         }
-    //     }
-    //     for(int i=0;i<V;i++){
-    //         if (dist[i]==1e9){
-    //             dist[i]=0;
-    //         }
-    //     }
-    //     return dist;
-    // }
+    }
 };
 
 
