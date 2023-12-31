da37e4a93a7baefdea0345e29dcf8eefb64e147d
Bubhoosra <120300546+Bubhoosra@users.noreply.github.com>
2023-12-31 17:57:06 +0530

    Updated Solution - GfG to GitHub

--- Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
+++ Medium/Implementing Dijkstra Algorithm/implementing-dijkstra-algorithm.cpp
@@ -10,24 +10,21 @@ class Solution
     //from the source vertex S.
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        set<pair<int,int>>s;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
         vector<int>dist(V,1e9);
-        s.insert({0,S});
         dist[S]=0;
-        while(!s.empty()){
-            auto it=*(s.begin());
-            int node=it.second;
-            int dis=it.first;
-            s.erase(it);
+        pq.push({0,S});
+        while(!pq.empty()){
+            int dis=pq.top().first;
+            int node=pq.top().second;
+            pq.pop();
             for(auto it:adj[node]){
-                int adjNode=it[0];
-                int edge=it[1];
-                if(dis+edge<dist[adjNode]){
-                    if(dist[adjNode]!=1e9){
-                        s.erase({dist[adjNode],adjNode});
-                    }
-                    dist[adjNode]=dis+edge;
-                    s.insert({dist[adjNode],adjNode});
+                int adjnode=it[0];
+                int adjweight=it[1];
+                
+                if(dis+adjweight<dist[adjnode]){
+                    dist[adjnode]=dis+adjweight;
+                    pq.push({dist[adjnode],adjnode});
                 }
             }
         }
